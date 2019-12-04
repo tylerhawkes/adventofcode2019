d40af297871cf014f71e2c8296f95f482ce37221
Tyler Hawkes <tyler.hawkes@gmail.com>
2019-12-03 23:03:54 -0700

    Maybe more elegant solution to day04

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,33 +1,23 @@
+#[cfg(not(any(feature = "part-one", feature = "part-two")))]
+compile_error!("Choose feature part-one or part-two");
+
 fn main() {
   let count = (284639..=748759)
     .filter(|i| {
-      let a = i / 100_000 % 10;
-      let b = i / 10_000 % 10;
-      let c = i / 1_000 % 10;
-      let d = i / 100 % 10;
-      let e = i / 10 % 10;
-      let f = i % 10;
-      let increasing = a <= b && b <= c && c <= d && d <= e && e <= f;
-
-      let double = if cfg!(feature = "part-one") {
-        a == b || b == c || c == d || d == e || e == f
-      } else if cfg!(feature = "part-two") {
-        let ad = (a == b && b != c);
-        let bd = (a != b && b == c && c != d);
-        let cd = (b != c && c == d && d != e);
-        let dd = (c != d && d == e && e != f);
-        let ed = (e != d && e == f);
-        match (ad, bd, cd, dd, ed) {
-          (true, false, _, _, _) => true,
-          (false, true, false, _, _) => true,
-          (_, false, true, false, _) => true,
-          (_, _, false, true, false) => true,
-          (_, _, _, false, true) => true,
-          _ => false,
-        }
-      } else {
-        panic!("Please choose feature 'part-one' or 'part-two'");
-      };
+      let digits = (0..6)
+        .rev()
+        .map(|b| i / 10_u32.pow(b) % 10)
+        .collect::<Vec<_>>();
+      let increasing = digits.windows(2).all(|d| d[0] <= d[1]);
+      #[cfg(feature = "part-one")]
+      let double = digits.windows(2).any(|d| d[0] == d[1]);
+      #[cfg(feature = "part-two")]
+      // Check ends then the middle 3
+      let double = (digits[0] == digits[1] && digits[1] != digits[2])
+        || (digits[3] != digits[4] && digits[4] == digits[5])
+        || digits
+          .windows(4)
+          .any(|d| d[0] != d[1] && d[1] == d[2] && d[2] != d[3]);
       increasing && double
     })
     .count();
