998ac20e4b57cb2c9798e480313b8fdf418ec571
Tyler Hawkes <tyler.hawkes@gmail.com>
2019-12-08 23:02:37 -0700

    Day09 finished

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,1 +1,283 @@
-fn main() {}
+use std::str::FromStr;
+use std::time::Instant;
+
+fn main() {
+  let start = Instant::now();
+  let codes =
+    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/day09.txt")).replace("\n", "");
+
+  let input = if cfg!(feature = "part-one") {
+    1
+  } else if cfg!(feature = "part-two") {
+    2
+  } else {
+    panic!("Please set feature 'part-one' or 'part-two")
+  };
+  let mut program = codes.parse::<Program>().unwrap().name("a").i(input);
+  program.ensure_space(1_000_000);
+  let output = program.run();
+  println!("Output: {:?}", output);
+  println!("Took {:?}", start.elapsed());
+}
+
+type I = i128;
+
+#[derive(Clone, Debug)]
+struct Program {
+  name: String,
+  codes: Vec<I>,
+  input: Vec<I>,
+  output: Vec<I>,
+  position: usize,
+  relative_position: I,
+}
+
+impl Program {
+  fn name(mut self, s: impl Into<String>) -> Self {
+    self.name = s.into();
+    self
+  }
+  fn run(&mut self) -> &[I] {
+    loop {
+      let opcode = (&self.codes[self.position..(self.position + 4).min(self.codes.len())]).into();
+      if let OpCode::Break = opcode {
+        break;
+      }
+      self.apply(opcode);
+      self.move_position(opcode);
+    }
+    self.output.as_slice()
+  }
+  fn move_position(&mut self, opcode: OpCode) {
+    let codes = self.codes.as_slice();
+    match opcode {
+      OpCode::Add(_, _, _) | OpCode::Multiply(_, _, _) => self.position += 4,
+      OpCode::Input(_) | OpCode::Output(_) | OpCode::AdjustRelativeBase(_) => self.position += 2,
+      OpCode::Break => unreachable!(),
+      OpCode::LessThan(_, _, _) | OpCode::Equals(_, _, _) => self.position += 4,
+      OpCode::JumpIfTrue(p1, p2) => {
+        if p1.value(codes, self.relative_position) != 0 {
+          self.position = p2.value(codes, self.relative_position) as usize;
+        } else {
+          self.position += 3;
+        }
+      }
+      OpCode::JumpIfFalse(p1, p2) => {
+        if p1.value(codes, self.relative_position) == 0 {
+          self.position = p2.value(codes, self.relative_position) as usize;
+        } else {
+          self.position += 3
+        }
+      }
+    };
+    //println!("{}: Moving position to {}", self.name, self.position);
+  }
+  fn apply(&mut self, opcode: OpCode) -> Option<I> {
+    //println!(
+    //  "{}: Applying {:?} (len: {})",
+    //  self.name,
+    //  opcode,
+    //  self.codes.len(),
+    //);
+    let relative_position = self.relative_position;
+    match opcode {
+      OpCode::Add(p1, p2, p3) => {
+        let p3_position = p3.position(relative_position);
+        self.ensure_space(p3_position);
+        self.codes[p3_position] =
+          p1.value(&self.codes, relative_position) + p2.value(&self.codes, relative_position)
+      }
+      OpCode::Multiply(p1, p2, p3) => {
+        let p3_position = p3.position(relative_position);
+        self.ensure_space(p3_position);
+        self.codes[p3_position] =
+          p1.value(&self.codes, relative_position) * p2.value(&self.codes, relative_position)
+      }
+      OpCode::Input(p1) => {
+        let p1_position = p1.position(relative_position);
+        self.ensure_space(p1_position);
+        self.codes[p1_position] = self.input.remove(0)
+      }
+      OpCode::Output(p1) => {
+        self.output.push(p1.value(&self.codes, relative_position));
+        return Some(p1.value(&self.codes, relative_position));
+      }
+      OpCode::Break => unreachable!(),
+      OpCode::JumpIfTrue(_, _) | OpCode::JumpIfFalse(_, _) => {}
+      OpCode::LessThan(p1, p2, p3) => {
+        let p3_position = p3.position(relative_position);
+        self.ensure_space(p3_position);
+        self.codes[p3_position] =
+          if p1.value(&self.codes, relative_position) < p2.value(&&self.codes, relative_position) {
+            1
+          } else {
+            0
+          }
+      }
+      OpCode::Equals(p1, p2, p3) => {
+        let p3_position = p3.position(relative_position);
+        self.ensure_space(p3_position);
+        self.codes[p3_position] =
+          if p1.value(&self.codes, relative_position) == p2.value(&self.codes, relative_position) {
+            1
+          } else {
+            0
+          }
+      }
+      OpCode::AdjustRelativeBase(p1) => {
+        self.relative_position += p1.value(&self.codes, relative_position)
+      }
+    }
+    None
+  }
+  fn ensure_space(&mut self, position: usize) {
+    if self.codes.len() <= position {
+      //println!("Extending from len {} to {}", self.codes.len(), position);
+      self.codes.reserve(position - self.codes.len());
+      while self.codes.len() <= position {
+        self.codes.push(0);
+      }
+    }
+  }
+  fn i(mut self, i: I) -> Self {
+    self.input.push(i);
+    self
+  }
+}
+
+impl FromStr for Program {
+  type Err = ();
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let codes = s
+      .split(",")
+      .filter_map(|s| {
+        s.parse::<I>()
+          .map_err(|_| println!("Unable to parse {}", s))
+          .ok()
+      })
+      .collect::<Vec<_>>();
+    Ok(Self {
+      name: "base".into(),
+      codes,
+      input: vec![],
+      output: vec![],
+      position: 0,
+      relative_position: 0,
+    })
+  }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum ParameterMode {
+  Position(I),
+  Immediate(I),
+  Relative(I),
+}
+
+impl ParameterMode {
+  fn value(self, codes: &[I], relative_position: I) -> I {
+    let value = match self {
+      Self::Position(i) => codes[i as usize],
+      Self::Immediate(i) => i,
+      Self::Relative(i) => codes[(i + relative_position) as usize],
+    };
+    //println!("Value of {:?} ({}) is {}", self, relative_position, value);
+    value
+  }
+  fn position(self, relative_position: I) -> usize {
+    let position = match self {
+      Self::Position(i) | Self::Immediate(i) => i as usize,
+      Self::Relative(i) => (i + relative_position) as usize,
+    };
+    //println!("Position of {:?} is {}", self, position);
+    position
+  }
+}
+
+impl From<(I, I)> for ParameterMode {
+  fn from((mode, value): (I, I)) -> Self {
+    match mode {
+      0 => Self::Position(value),
+      1 => Self::Immediate(value),
+      2 => Self::Relative(value),
+      _ => unreachable!(),
+    }
+  }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum OpCode {
+  Add(ParameterMode, ParameterMode, ParameterMode),
+  Multiply(ParameterMode, ParameterMode, ParameterMode),
+  Input(ParameterMode),
+  Output(ParameterMode),
+  Break,
+  JumpIfTrue(ParameterMode, ParameterMode),
+  JumpIfFalse(ParameterMode, ParameterMode),
+  LessThan(ParameterMode, ParameterMode, ParameterMode),
+  Equals(ParameterMode, ParameterMode, ParameterMode),
+  AdjustRelativeBase(ParameterMode),
+}
+
+impl From<&[I]> for OpCode {
+  fn from(codes: &[I]) -> Self {
+    //println!("Making OpCode from {:?}", codes);
+    let i = codes[0];
+    let code = i % 100;
+    let param1 = i / 100 % 10;
+    let param2 = i / 1000 % 10;
+    let param3 = i / 10000 % 10;
+    match code {
+      1 => Self::Add(
+        (param1, codes[1]).into(),
+        (param2, codes[2]).into(),
+        (param3, codes[3]).into(),
+      ),
+      2 => Self::Multiply(
+        (param1, codes[1]).into(),
+        (param2, codes[2]).into(),
+        (param3, codes[3]).into(),
+      ),
+      3 => Self::Input((param1, codes[1]).into()),
+      4 => Self::Output((param1, codes[1]).into()),
+      5 => Self::JumpIfTrue((param1, codes[1]).into(), (param2, codes[2]).into()),
+      6 => Self::JumpIfFalse((param1, codes[1]).into(), (param2, codes[2]).into()),
+      7 => Self::LessThan(
+        (param1, codes[1]).into(),
+        (param2, codes[2]).into(),
+        (param3, codes[3]).into(),
+      ),
+      8 => Self::Equals(
+        (param1, codes[1]).into(),
+        (param2, codes[2]).into(),
+        (param3, codes[3]).into(),
+      ),
+      9 => Self::AdjustRelativeBase((param1, codes[1]).into()),
+      99 => Self::Break,
+      _ => unreachable!(),
+    }
+  }
+}
+
+#[test]
+fn test_program() {
+  let mut program1 = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
+    .parse::<Program>()
+    .unwrap();
+
+  assert_eq!(
+    program1.run(),
+    &[109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
+  );
+
+  panic!("failing");
+
+  let mut program2 = "1102,34915192,34915192,7,4,7,99,0"
+    .parse::<Program>()
+    .unwrap();
+  assert_eq!(program2.run()[0].to_string().len(), 16);
+
+  let mut program3 = "104,1125899906842624,99".parse::<Program>().unwrap();
+  assert_eq!(program3.run()[0], 1125899906842624);
+}
