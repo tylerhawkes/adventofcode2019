bb159890db2d1fb6c031fcf6a7da9a7017599a8c
Tyler Hawkes <tyler.hawkes@gmail.com>
2019-12-14 22:37:15 -0700

    Day13 finished

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,1 +1,375 @@
-fn main() {}
+use std::collections::HashMap;
+use std::fmt::{Display, Error, Formatter};
+use std::str::FromStr;
+
+fn main() {
+  let input =
+    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/day13.txt")).replace("\n", "");
+  let mut program = input.parse::<Program<Game>>().unwrap();
+  program.ensure_space(1_000_000);
+
+  #[cfg(feature = "part-one")]
+  {
+    let block_tiles = program.run();
+    println!("block tiles: {}", program.system.count_tiles(Tile::Block));
+  }
+  #[cfg(feature = "part-two")]
+  {
+    program.codes[0] = 2;
+    program.run();
+    println!("Score: {}", program.system.score);
+  }
+}
+
+#[derive(Debug, Clone)]
+struct Game {
+  screen: HashMap<(I, I), Tile>,
+  score: I,
+  paddle: (I, I),
+  ball: (I, I),
+  program_output: Vec<I>,
+}
+
+impl Game {
+  fn apply_output(&mut self, output: I) {
+    self.program_output.push(output);
+    if self.program_output.len() % 3 != 0 {
+      return;
+    }
+    let mut output = self.program_output.drain(..);
+    while let (Some(x), Some(y), Some(tile)) = (output.next(), output.next(), output.next()) {
+      if x == -1 && y == 0 {
+        println!("Updating score to: {}", tile);
+        self.score = tile;
+        continue;
+      }
+      let tile = Tile::from(tile);
+      self.screen.insert((x, y), tile);
+      match tile {
+        Tile::HorizontalPaddle => self.paddle = (x, y),
+        Tile::Ball => self.ball = (x, y),
+        _ => {}
+      }
+    }
+  }
+  fn count_tiles(&self, tile: Tile) -> usize {
+    self.screen.values().filter(|t| **t == tile).count()
+  }
+  fn print(&self) {
+    if self.screen.is_empty() {
+      return;
+    }
+    let xmin = self.screen.keys().map(|k| k.0).min().unwrap();
+    let ymin = self.screen.keys().map(|k| k.1).min().unwrap();
+    let xmax = self.screen.keys().map(|k| k.0).max().unwrap();
+    let ymax = self.screen.keys().map(|k| k.1).max().unwrap();
+    println!("bounds: {}, {}, {}, {}", xmin, ymin, xmax, ymax);
+    for y in ymin..=ymax {
+      for x in xmin..=xmax {
+        print!("{}", self.screen.get(&(x, y)).unwrap_or(&Tile::Empty));
+      }
+      println!();
+    }
+  }
+}
+
+impl Default for Game {
+  fn default() -> Self {
+    Self {
+      screen: HashMap::with_capacity(1000),
+      score: 0,
+      paddle: (0, 0),
+      ball: (0, 0),
+      program_output: Vec::with_capacity(10000),
+    }
+  }
+}
+
+impl System for Game {
+  fn send_input(&mut self) -> i64 {
+    let input = (self.ball.0 - self.paddle.0).signum();
+    //println!("returning input: {}", input);
+    input
+  }
+
+  fn take_output(&mut self, output: i64) {
+    self.apply_output(output);
+  }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum Tile {
+  Empty,
+  Wall,
+  Block,
+  HorizontalPaddle,
+  Ball,
+}
+
+impl From<I> for Tile {
+  fn from(i: i64) -> Self {
+    match i {
+      0 => Self::Empty,
+      1 => Self::Wall,
+      2 => Self::Block,
+      3 => Self::HorizontalPaddle,
+      4 => Self::Ball,
+      _ => unreachable!(),
+    }
+  }
+}
+
+impl Display for Tile {
+  fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+    let c = match self {
+      Self::Empty => " ",
+      Self::Wall => "|",
+      Self::Block => "#",
+      Self::HorizontalPaddle => "_",
+      Self::Ball => "O",
+    };
+    c.fmt(f)
+  }
+}
+
+type I = i64;
+
+trait System: Default {
+  fn send_input(&mut self) -> I;
+  fn take_output(&mut self, output: I);
+}
+
+#[derive(Clone, Debug)]
+struct Program<S: System> {
+  name: String,
+  codes: Vec<I>,
+  position: usize,
+  relative_position: I,
+  system: S,
+}
+
+impl<S: System> Program<S> {
+  fn name(mut self, s: impl Into<String>) -> Self {
+    self.name = s.into();
+    self
+  }
+  fn run(&mut self) -> Option<I> {
+    loop {
+      let opcode = (&self.codes[self.position..(self.position + 4).min(self.codes.len())]).into();
+      if let OpCode::Break = opcode {
+        return None;
+      }
+      let output = self.apply(opcode);
+      self.move_position(opcode);
+      //      if let Some(i) = output {
+      //        return Some(i);
+      //      }
+    }
+  }
+  fn move_position(&mut self, opcode: OpCode) {
+    let codes = self.codes.as_slice();
+    match opcode {
+      OpCode::Add(_, _, _) | OpCode::Multiply(_, _, _) => self.position += 4,
+      OpCode::Input(_) | OpCode::Output(_) | OpCode::AdjustRelativeBase(_) => self.position += 2,
+      OpCode::Break => unreachable!(),
+      OpCode::LessThan(_, _, _) | OpCode::Equals(_, _, _) => self.position += 4,
+      OpCode::JumpIfTrue(p1, p2) => {
+        if p1.value(codes, self.relative_position) != 0 {
+          self.position = p2.value(codes, self.relative_position) as usize;
+        } else {
+          self.position += 3;
+        }
+      }
+      OpCode::JumpIfFalse(p1, p2) => {
+        if p1.value(codes, self.relative_position) == 0 {
+          self.position = p2.value(codes, self.relative_position) as usize;
+        } else {
+          self.position += 3
+        }
+      }
+    };
+    //println!("{}: Moving position to {}", self.name, self.position);
+  }
+  fn apply(&mut self, opcode: OpCode) -> Option<I> {
+    //println!(
+    //  "{}: Applying {:?} (len: {})",
+    //  self.name,
+    //  opcode,
+    //  self.codes.len(),
+    //);
+    let relative_position = self.relative_position;
+    match opcode {
+      OpCode::Add(p1, p2, p3) => {
+        let p3_position = p3.position(relative_position);
+        self.ensure_space(p3_position);
+        self.codes[p3_position] =
+          p1.value(&self.codes, relative_position) + p2.value(&self.codes, relative_position)
+      }
+      OpCode::Multiply(p1, p2, p3) => {
+        let p3_position = p3.position(relative_position);
+        self.ensure_space(p3_position);
+        self.codes[p3_position] =
+          p1.value(&self.codes, relative_position) * p2.value(&self.codes, relative_position)
+      }
+      OpCode::Input(p1) => {
+        let p1_position = p1.position(relative_position);
+        self.ensure_space(p1_position);
+        self.codes[p1_position] = self.system.send_input();
+      }
+      OpCode::Output(p1) => {
+        let value = p1.value(&self.codes, relative_position);
+        self.system.take_output(value);
+        return Some(p1.value(&self.codes, relative_position));
+      }
+      OpCode::Break => unreachable!(),
+      OpCode::JumpIfTrue(_, _) | OpCode::JumpIfFalse(_, _) => {}
+      OpCode::LessThan(p1, p2, p3) => {
+        let p3_position = p3.position(relative_position);
+        self.ensure_space(p3_position);
+        self.codes[p3_position] =
+          if p1.value(&self.codes, relative_position) < p2.value(&&self.codes, relative_position) {
+            1
+          } else {
+            0
+          }
+      }
+      OpCode::Equals(p1, p2, p3) => {
+        let p3_position = p3.position(relative_position);
+        self.ensure_space(p3_position);
+        self.codes[p3_position] =
+          if p1.value(&self.codes, relative_position) == p2.value(&self.codes, relative_position) {
+            1
+          } else {
+            0
+          }
+      }
+      OpCode::AdjustRelativeBase(p1) => {
+        self.relative_position += p1.value(&self.codes, relative_position)
+      }
+    }
+    None
+  }
+  fn ensure_space(&mut self, position: usize) {
+    if self.codes.len() <= position {
+      //println!("Extending from len {} to {}", self.codes.len(), position);
+      self.codes.reserve(position - self.codes.len());
+      while self.codes.len() <= position {
+        self.codes.push(0);
+      }
+    }
+  }
+}
+
+impl<S: System> FromStr for Program<S> {
+  type Err = ();
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let codes = s
+      .split(",")
+      .filter_map(|s| {
+        s.parse::<I>()
+          .map_err(|_| println!("Unable to parse {}", s))
+          .ok()
+      })
+      .collect::<Vec<_>>();
+    Ok(Self {
+      name: "base".into(),
+      codes,
+      position: 0,
+      relative_position: 0,
+      system: S::default(),
+    })
+  }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum ParameterMode {
+  Position(I),
+  Immediate(I),
+  Relative(I),
+}
+
+impl ParameterMode {
+  fn value(self, codes: &[I], relative_position: I) -> I {
+    let value = match self {
+      Self::Position(i) => codes[i as usize],
+      Self::Immediate(i) => i,
+      Self::Relative(i) => codes[(i + relative_position) as usize],
+    };
+    //println!("Value of {:?} ({}) is {}", self, relative_position, value);
+    value
+  }
+  fn position(self, relative_position: I) -> usize {
+    let position = match self {
+      Self::Position(i) | Self::Immediate(i) => i as usize,
+      Self::Relative(i) => (i + relative_position) as usize,
+    };
+    //println!("Position of {:?} is {}", self, position);
+    position
+  }
+}
+
+impl From<(I, I)> for ParameterMode {
+  fn from((mode, value): (I, I)) -> Self {
+    match mode {
+      0 => Self::Position(value),
+      1 => Self::Immediate(value),
+      2 => Self::Relative(value),
+      _ => unreachable!(),
+    }
+  }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum OpCode {
+  Add(ParameterMode, ParameterMode, ParameterMode),
+  Multiply(ParameterMode, ParameterMode, ParameterMode),
+  Input(ParameterMode),
+  Output(ParameterMode),
+  Break,
+  JumpIfTrue(ParameterMode, ParameterMode),
+  JumpIfFalse(ParameterMode, ParameterMode),
+  LessThan(ParameterMode, ParameterMode, ParameterMode),
+  Equals(ParameterMode, ParameterMode, ParameterMode),
+  AdjustRelativeBase(ParameterMode),
+}
+
+impl From<&[I]> for OpCode {
+  fn from(codes: &[I]) -> Self {
+    //println!("Making OpCode from {:?}", codes);
+    let i = codes[0];
+    let code = i % 100;
+    let param1 = i / 100 % 10;
+    let param2 = i / 1000 % 10;
+    let param3 = i / 10000 % 10;
+    match code {
+      1 => Self::Add(
+        (param1, codes[1]).into(),
+        (param2, codes[2]).into(),
+        (param3, codes[3]).into(),
+      ),
+      2 => Self::Multiply(
+        (param1, codes[1]).into(),
+        (param2, codes[2]).into(),
+        (param3, codes[3]).into(),
+      ),
+      3 => Self::Input((param1, codes[1]).into()),
+      4 => Self::Output((param1, codes[1]).into()),
+      5 => Self::JumpIfTrue((param1, codes[1]).into(), (param2, codes[2]).into()),
+      6 => Self::JumpIfFalse((param1, codes[1]).into(), (param2, codes[2]).into()),
+      7 => Self::LessThan(
+        (param1, codes[1]).into(),
+        (param2, codes[2]).into(),
+        (param3, codes[3]).into(),
+      ),
+      8 => Self::Equals(
+        (param1, codes[1]).into(),
+        (param2, codes[2]).into(),
+        (param3, codes[3]).into(),
+      ),
+      9 => Self::AdjustRelativeBase((param1, codes[1]).into()),
+      99 => Self::Break,
+      _ => unreachable!(),
+    }
+  }
+}
--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,1 +1,297 @@
-fn main() {}
+use std::cmp::Ordering;
+use std::collections::hash_map::Entry;
+use std::collections::{HashMap, HashSet};
+use std::hash::{Hash, Hasher};
+use std::ops::{Add, AddAssign, Sub, SubAssign};
+use std::sync::Arc;
+
+fn main() {
+  let input = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/day14.txt"));
+  let mut fuel_calc = FuelCalculator::from_str(input);
+  let units_for_fuel = fuel_calc.calculate_ore_for_fuel(1);
+  println!("{:?}", units_for_fuel);
+  let fuel_from_1_trillion_ore = fuel_calc.calculate_fuel_for_1_trillion_ore();
+  println!("{:?}", fuel_from_1_trillion_ore);
+}
+
+#[derive(Copy, Clone, Debug)]
+struct ChemicalUnit<'a> {
+  name: &'a str,
+  units: u64,
+}
+
+impl<'a> ChemicalUnit<'a> {
+  fn is_ore(&self) -> bool {
+    self.name == "ORE"
+  }
+  fn with_units(&self, units: u64) -> Self {
+    Self {
+      name: self.name,
+      units,
+    }
+  }
+  fn from_str(s: &'a str) -> Self {
+    let mut v = s.split(" ").filter(|p| p != &"");
+    let units = v.next().unwrap().parse::<u64>().unwrap();
+    let name = v.next().unwrap();
+    Self { name, units }
+  }
+}
+
+impl<'a> Hash for ChemicalUnit<'a> {
+  fn hash<H: Hasher>(&self, state: &mut H) {
+    self.name.hash(state)
+  }
+}
+
+impl<'a> PartialEq for ChemicalUnit<'a> {
+  fn eq(&self, other: &Self) -> bool {
+    self.name.eq(other.name)
+  }
+}
+
+impl<'a> Eq for ChemicalUnit<'a> {}
+
+impl<'a> Ord for ChemicalUnit<'a> {
+  fn cmp(&self, other: &Self) -> Ordering {
+    self.name.cmp(other.name)
+  }
+}
+
+impl<'a> PartialOrd for ChemicalUnit<'a> {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.cmp(other))
+  }
+}
+
+#[derive(Debug, Clone)]
+struct FuelCalculator<'a> {
+  map: Arc<HashMap<&'a str, (ChemicalUnit<'a>, Vec<ChemicalUnit<'a>>)>>,
+  available: HashMap<&'a str, u64>,
+  ore_used: u64,
+  fuel_created: u64,
+}
+
+impl<'a> FuelCalculator<'a> {
+  //  fn calculate_fuel_for_1_trillion_ore(&mut self) -> u64 {
+  //    self.available.clear();
+  //    let mut ore_per_fuel = Vec::with_capacity(10000);
+  //    let mut ore_used = 0;
+  //    let num_to_check = 50;
+  //    let mut continue_from = loop {
+  //      let ore_for_fuel = self.calculate_ore_for_fuel();
+  //      ore_per_fuel.push(ore_for_fuel);
+  //      let len = ore_per_fuel.len();
+  //      if len > num_to_check + 1 {
+  //        if ore_per_fuel[0..num_to_check] == ore_per_fuel[len - num_to_check..len] {
+  //          break 0;
+  //        }
+  //      }
+  //      if len == 5000 {
+  //        let total_ore = ore_per_fuel.iter().sum::<u64>();
+  //        let partial = 1_000_000_000_000.0_f64 / total_ore as f64;
+  //        println!(
+  //          "total_ore = {}, partial = {}, return = {}, previous = 3281755 (too low)",
+  //          total_ore,
+  //          partial,
+  //          partial * len as f64
+  //        );
+  //        return (partial * len as f64) as u64;
+  //      }
+  //      if len % 100 == 0 {
+  //        ore_used += ore_per_fuel[len - 100..len].iter().sum::<u64>();
+  //        println!(
+  //          "Running loop: {}, ore_used: {},  {:?}",
+  //          len,
+  //          ore_used,
+  //          &ore_per_fuel[len - 100..len]
+  //        );
+  //      }
+  //    };
+  //    //println!("{}, {:#?}", ore_per_fuel.len(), ore_per_fuel);
+  //    let ore_per_loop = ore_per_fuel[..ore_per_fuel.len() - num_to_check]
+  //      .iter()
+  //      .sum::<u64>();
+  //    let loops = 1_000_000_000_000 / ore_per_loop;
+  //    let mut fuel = (ore_per_fuel.len() - num_to_check) as u64 * loops;
+  //    let mut leftover = (1_000_000_000_000 - (ore_per_loop * loops)) as i64;
+  //    while leftover >= 0 {
+  //      let ore_per_fuel = ore_per_fuel[continue_from];
+  //      leftover -= ore_per_fuel as i64;
+  //      fuel += 1;
+  //      continue_from += 1;
+  //    }
+  //    fuel - 1
+  //  }
+  fn calculate_fuel_for_1_trillion_ore(&mut self) -> u64 {
+    const TRILLION: u64 = 1_000_000_000_000;
+    let ore = self.calculate_ore_for_fuel(10);
+    let mut min = TRILLION / (ore * 100);
+    let mut max = TRILLION / (ore / 100);
+    let mut iter = 0;
+    while min + 1 != max {
+      println!("min: {}, max: {}, iter: {}", min, max, iter);
+      let mid = (min + max) / 2;
+      let ore = self.calculate_ore_for_fuel(mid);
+      if ore < TRILLION {
+        min = mid;
+      } else {
+        max = mid;
+      }
+      iter += 1;
+    }
+    min
+  }
+  fn calculate_ore_for_fuel(&mut self, number_of_fuel: u64) -> u64 {
+    self.available.clear();
+    let needed = self.calculate_recipe_for_chemical_unit(&ChemicalUnit {
+      name: "FUEL",
+      units: number_of_fuel,
+    });
+    needed
+  }
+  fn calculate_recipe_for_chemical_unit(&mut self, needed: &ChemicalUnit<'a>) -> u64 {
+    if needed.is_ore() {
+      //println!("returning {} ore units", needed.units);
+      return needed.units;
+    }
+    let available = match self.available.entry(needed.name) {
+      Entry::Occupied(mut o) => *o.get(),
+      Entry::Vacant(mut v) => 0,
+    };
+    let map_clone = self.map.clone();
+    let mut recipe_units = 0;
+    let mut needed_ore = 0;
+    let (recipe, recipe_chemicals) = map_clone.get(needed.name).unwrap();
+    while recipe_units + available < needed.units {
+      let scale = (((needed.units - recipe_units) / recipe.units).max(1) - 1).max(1);
+      needed_ore += recipe_chemicals
+        .into_iter()
+        .map(|recipe_chemical| {
+          self.calculate_recipe_for_chemical_unit(
+            &recipe_chemical.with_units(recipe_chemical.units * scale),
+          )
+        })
+        .sum::<u64>();
+      recipe_units += recipe.units * scale;
+    }
+    if recipe_units + available >= needed.units {
+      match self.available.entry(needed.name) {
+        Entry::Occupied(mut o) => {
+          *o.get_mut() = recipe_units + available - needed.units;
+        }
+        Entry::Vacant(mut v) => {
+          v.insert(recipe_units + available - needed.units);
+        }
+      }
+    }
+    //println!("Created {} units for {:?}", recipe_units, needed);
+    needed_ore
+  }
+
+  fn from_str(s: &'a str) -> Self {
+    let map = s
+      .lines()
+      .map(|l| {
+        let mut v = l.split("=>");
+        let sources = v
+          .next()
+          .unwrap()
+          .split(",")
+          .map(|c| ChemicalUnit::from_str(c))
+          .collect::<Vec<_>>();
+        let chemical = ChemicalUnit::from_str(v.next().unwrap());
+        (chemical.name, (chemical, sources))
+      })
+      .collect::<HashMap<_, _>>();
+    Self {
+      available: HashMap::with_capacity(map.len()),
+      map: Arc::new(map),
+      ore_used: 0,
+      fuel_created: 0,
+    }
+  }
+}
+
+impl<'a> From<&'a str> for FuelCalculator<'a> {
+  fn from(s: &'a str) -> Self {
+    Self::from_str(s)
+  }
+}
+
+#[test]
+fn test_ore_to_fuel() {
+  let mut fuel_calc: FuelCalculator = "10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL"
+    .into();
+
+  //println!("{:#?}", fuel_calc);
+  //assert_eq!(31, fuel_calc.calculate_ore_for_fuel());
+
+  fuel_calc = "9 ORE => 2 A
+8 ORE => 3 B
+7 ORE => 5 C
+3 A, 4 B => 1 AB
+5 B, 7 C => 1 BC
+4 C, 1 A => 1 CA
+2 AB, 3 BC, 4 CA => 1 FUEL"
+    .into();
+
+  //println!("{:#?}", fuel_calc);
+  assert_eq!(165, fuel_calc.calculate_ore_for_fuel(1));
+
+  fuel_calc = "157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF 
+177 ORE => 5 HKGWZ 
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF 
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"
+    .into();
+  //println!("{:#?}", fuel_calc);
+  assert_eq!(13312, fuel_calc.calculate_ore_for_fuel(1));
+  assert_eq!(82892753, fuel_calc.calculate_fuel_for_1_trillion_ore());
+
+  fuel_calc = "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF"
+    .into();
+  assert_eq!(180697, fuel_calc.calculate_ore_for_fuel(1));
+  assert_eq!(5586022, fuel_calc.calculate_fuel_for_1_trillion_ore());
+
+  fuel_calc = "171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX"
+    .into();
+  assert_eq!(2210736, fuel_calc.calculate_ore_for_fuel(1));
+  assert_eq!(460664, fuel_calc.calculate_fuel_for_1_trillion_ore());
+}
